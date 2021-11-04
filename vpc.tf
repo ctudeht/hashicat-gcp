1654333ab3fbc1adc84342e5e3a75a92a0179bee
Denys Pavlov <ctudeht@gmail.com>
2021-11-04 16:42:45 +0300

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "network" {
   source  = "app.terraform.io/CtudehtCo/network/google"
   version = "3.4.0"
-  name = "guarav-network"
+  network_name = "guarav-network"
   project_id = var.project
   subnets = [
   {
